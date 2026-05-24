2bc0ba24452d58f02cfbb9e82675c941fe0bc021
speranzarust <speranzarust@gmail.com>
2026-05-24 19:23:37 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
-  theme: { extend: {} },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        background: "#05070b",
+        foreground: "#ffffff",
+      },
+    },
+  },
+
   plugins: [],
-}
-export default config
+};
+
+export default config;
